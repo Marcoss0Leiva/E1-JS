734512926eb231bf2c3c6fbfbede07c854d36660
Leiva Marcos <marcos001leiva@gmail.com>
2023-07-08 18:42:57 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,124 @@
+ /*Ejercicio 1 
+
+ function verificarSiEsPar(numero) {
+    if (numero % 2 === 0) {
+      console.log(numero + " es un número par.");
+    } else {
+      console.log(numero + " es un número impar.");
+    }
+  }
+  
+  // Ejemplos de uso
+ verificarSiEsPar(7);  //  7 es un número impar.
+ verificarSiEsPar(12); //  12 es un número par.
+*/
+ 
+/*Ejercici 2
+
+
+    function compararNumeros(numero1, numero2) {
+    if (numero1 > numero2) {
+      console.log(numero1 + " es mayor que " + numero2);
+    } else if (numero1 < numero2) {
+      console.log(numero2 + " es mayor que " + numero1);
+    } else {
+      console.log("Ambos números son iguales.");
+    }
+  }
+  
+  // Ejemplos de uso
+  compararNumeros(4, 7);   //  7 es mayor que 4
+  compararNumeros(10, 3);  //  10 es mayor que 3
+  compararNumeros(5, 5);   //  Ambos números son iguales.
+*/
+
+ 
+ /*Ejercici 3
+
+  function verificarMultiplo(numero) {
+    if (numero % 5 === 0) {
+      console.log(numero + " es múltiplo de 5.");
+    } else {
+      console.log(numero + " no es múltiplo de 5.");
+    }
+  }
+  
+  // Ejemplos de uso
+  verificarMultiplo(10);   //  10 es múltiplo de 5.
+  verificarMultiplo(7);    //  7 no es múltiplo de 5.
+  verificarMultiplo(25);   //  25 es múltiplo de 5.
+  verificarMultiplo(13);   //  13 no es múltiplo de 5.
+ */
+ 
+  /*Ejercici 4
+
+  function imprimirHasta(numero) {
+    for (let i = 0; i <= numero; i++) {
+      console.log(i);
+    }
+  }
+  
+  // Ejemplo de uso
+  imprimirHasta(5); // i va a recorrer del 0 al 5
+*/
+
+
+  /*Ejercici 5
+
+
+  function PalabraCantidad(palabra, cantidad) {
+    for (let i = 0; i < cantidad; i++) {
+      console.log(palabra);
+    }
+  }
+  
+  // Ejemplo de uso
+  PalabraCantidad("Hola", 5); //  imprimira 5 "Hola"
+*/
+  
+  
+  /*Ejercici 6
+
+
+  function imprimirArray(array) {
+    for (let i = 0; i < array.length; i++) {
+      console.log(array[i]);
+    }
+  }
+  
+  // Ejemplo de uso
+  const miArray = [1, 2, 3, 4, 5]; //se imprimiran los valores de los array
+  imprimirArray(miArray);
+*/
+
+  /*Ejercici 7
+
+  function imprimirArrayExceptoQuinto(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (i !== 4) {  // 4 representa la quinta posición (índice 4) en el array
+      console.log(array[i]);
+    }
+  }
+ }
+
+ // Ejemplo de uso
+ const miArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+ imprimirArrayExceptoQuinto(miArray);
+*/
+ 
+  /*Ejercici 8
+
+ function multiplicarArrayPorNumero(array, numero) {
+    for (let i = 0; i < array.length; i++) {
+      const resultado = array[i] * numero;
+      console.log(resultado);
+    }
+  }
+  
+  // Ejemplo de uso
+  const miArray = [1, 2, 3, 4, 5];
+  const numeroParametro = 2;
+  multiplicarArrayPorNumero(miArray, numeroParametro);
+*/
+
+  
